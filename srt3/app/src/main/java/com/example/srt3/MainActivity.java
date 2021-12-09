efbf2b403bb656046522e1a303c241f5cd9a79ca
kimsiyeon <pennya6@hs.ac.kr>
2021-12-09 23:01:33 +0900

    인원수 체크 xml 수정중

--- srt3/app/src/main/java/com/example/srt3/MainActivity.java
+++ srt3/app/src/main/java/com/example/srt3/MainActivity.java
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.chat:
-                //Toast.makeText(getApplicationContext(), "chatbot", Toast.LENGTH_SHORT).show();
                 showDialog();
                 return true;
             default:
@@ -55,9 +54,9 @@ public class MainActivity extends AppCompatActivity {
     // 챗봇 대화상자
     void showDialog(){
         AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this)
-                .setMessage("SRT챗봇은 카카오톡 기반의 고객 안내 서비스입니다. " +
-                        "상담원 연결이 필요한 고객님께서는 SR고객센터(1800-1472)를 이용해 주십시오." +
-                        "SRT챗봇으로 이동하시겠습니까?")
+                .setMessage("SRT챗봇은 카카오톡 기반의 고객 안내 서비스입니다. \n\n" +
+                        "상담원 연결이 필요한 고객님께서는 SR고객센터(1800-1472)를 이용해 주십시오. \n\n" +
+                        "SRT챗봇으로 이동하시겠습니까?\n")
                 .setPositiveButton("예", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {

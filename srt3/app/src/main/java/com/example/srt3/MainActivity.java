e647fad103dff828deb23815571690a31473268f
kimsiyeon <pennya6@hs.ac.kr>
2021-12-09 22:48:25 +0900

    대화상자 완료

--- srt3/app/src/main/java/com/example/srt3/MainActivity.java
+++ srt3/app/src/main/java/com/example/srt3/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.srt3;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -27,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("");
-        
+
     }
 
     @Override
@@ -41,10 +44,33 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.chat:
-                Toast.makeText(getApplicationContext(), "chatbot", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), "chatbot", Toast.LENGTH_SHORT).show();
+                showDialog();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    // 챗봇 대화상자
+    void showDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this)
+                .setMessage("SRT챗봇은 카카오톡 기반의 고객 안내 서비스입니다. " +
+                        "상담원 연결이 필요한 고객님께서는 SR고객센터(1800-1472)를 이용해 주십시오." +
+                        "SRT챗봇으로 이동하시겠습니까?")
+                .setPositiveButton("예", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        AlertDialog dialog=builder.create();
+        dialog.show();
     }
 }

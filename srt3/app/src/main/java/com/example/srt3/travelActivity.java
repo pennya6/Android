9fe72daf50bb9d243329085964ebb6f9bc18ceed
kimsiyeon <pennya6@hs.ac.kr>
2021-12-10 16:47:41 +0900

    srt, korail 버튼 선택 구현 완료

--- srt3/app/src/main/java/com/example/srt3/travelActivity.java
+++ srt3/app/src/main/java/com/example/srt3/travelActivity.java
@@ -7,12 +7,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class travelActivity extends AppCompatActivity {
     Toolbar toolbar;
+    ImageButton choice_srt,choice_korail;
+    boolean i=true;
+
     @Override
     protected void onCreate(Bundle bundle){
         super.onCreate(bundle);
@@ -21,6 +27,31 @@ public class travelActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar2);
         toolbar.setTitle("역선택");
         setSupportActionBar(toolbar);
+
+        choice_korail=(ImageButton) findViewById(R.id.imageButton5);
+        choice_srt=(ImageButton) findViewById(R.id.imageButton4);
+
+        choice_srt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(i==true){
+                    choice_srt.setImageResource(R.drawable.srt_2);
+                    choice_korail.setImageResource(R.drawable.korail_1);
+                    i=true;
+                }
+            }
+        });
+
+        choice_korail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(i==true){
+                    choice_srt.setImageResource(R.drawable.srt_1);
+                    choice_korail.setImageResource(R.drawable.korail_2);
+                    i=true;
+                }
+            }
+        });
 
     }
 

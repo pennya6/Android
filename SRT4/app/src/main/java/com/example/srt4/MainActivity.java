9ae120d9739ec8880ea513ff0f73998bf0b8f338
kimsiyeon <pennya6@hs.ac.kr>
2021-12-12 22:52:44 +0900

    SRT4 프래그먼트 연결 완료

--- SRT4/app/src/main/java/com/example/srt4/MainActivity.java
+++ SRT4/app/src/main/java/com/example/srt4/MainActivity.java
@@ -23,6 +23,8 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.io.FileReader;
 
@@ -44,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
     private fragment_second fragment_second=new fragment_second();
     private MainFragment fragment=new MainFragment();
+    private fragment_third fragment_third=new fragment_third();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,22 +55,47 @@ public class MainActivity extends AppCompatActivity {
 
         mytoolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(mytoolbar);
-        getSupportActionBar().setIcon(R.drawable.srt);
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("");
+        getSupportActionBar().setTitle("SRT");
 
 
-        FragmentTransaction transaction=fragmentManager.beginTransaction();
-        transaction.replace(R.id.container,fragment).commitAllowingStateLoss();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainFragment, fragment).commit();
+
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.bottom_ticketing:
+                        //getSupportActionBar().setIcon(R.drawable.srt);
+                        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                        getSupportActionBar().setTitle("SRT");
+                        getSupportFragmentManager().beginTransaction().replace(R.id.mainFragment,fragment).commit();
+                        return true;
+                    case R.id.bottom_register:
+                        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                        getSupportActionBar().setTitle("승차권확인");
+                        getSupportFragmentManager().beginTransaction().replace(R.id.mainFragment,fragment_second).commit();
+                        return true;
+                    case R.id.bottom_back:
+                        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                        getSupportActionBar().setTitle("정기/회수승차권발권");
+                        getSupportFragmentManager().beginTransaction().replace(R.id.mainFragment,fragment_third).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
 
 
-        //getSupportFragmentManager().beginTransaction().replace(R.id.mainFragment, fragment1).commit();
+
 
         imageButton=(ImageButton) findViewById(R.id.imageButton3);
-        imageButton.bringToFront();
+        //imageButton.bringToFront();
 
         mbtn=(Button) findViewById(R.id.minus_button1);
         nbtn=(TextView) findViewById(R.id.textView1);
@@ -93,179 +121,8 @@ public class MainActivity extends AppCompatActivity {
         nbtn6=(TextView) findViewById(R.id.textView6);
         pbtn6=(Button) findViewById(R.id.plus_button6);
 
-        mbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum<=0){
-                    nbtn.setText("0");
-                }else {
-                    curNum--;
-                    nbtn.setText(curNum+"");
-                }
-            }
-        });
-
-        pbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum>=20){
-                    nbtn.setText("20");
-                }else{
-                    curNum++;
-                    nbtn.setText(curNum+"");
-                }
-            }
-        });
-        mbtn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum2<=0){
-                    nbtn2.setText("0");
-                }else {
-                    curNum2--;
-                    nbtn2.setText(curNum2+"");
-                }
-            }
-        });
-        pbtn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum2>=20){
-                    nbtn2.setText("20");
-                }else{
-                    curNum2++;
-                    nbtn2.setText(curNum2+"");
-                }
-            }
-        });
-        mbtn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum3<=0){
-                    nbtn3.setText("0");
-                }else {
-                    curNum3--;
-                    nbtn3.setText(curNum3+"");
-                }
-            }
-        });
-        pbtn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum3>=20){
-                    nbtn3.setText("20");
-                }else{
-                    curNum3++;
-                    nbtn3.setText(curNum3+"");
-                }
-            }
-        });
-
-        mbtn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum4<=0){
-                    nbtn4.setText("0");
-                }else {
-                    curNum4--;
-                    nbtn4.setText(curNum4+"");
-                }
-            }
-        });
-        pbtn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum4>=20){
-                    nbtn4.setText("20");
-                }else{
-                    curNum4++;
-                    nbtn4.setText(curNum4+"");
-                }
-            }
-        });
-
-        mbtn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum5<=0){
-                    nbtn5.setText("0");
-                }else {
-                    curNum5--;
-                    nbtn5.setText(curNum5+"");
-                }
-            }
-        });
-        pbtn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum5>=20){
-                    nbtn5.setText("20");
-                }else{
-                    curNum5++;
-                    nbtn5.setText(curNum5+"");
-                }
-            }
-        });
-
-        mbtn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum6<=0){
-                    nbtn6.setText("0");
-                }else {
-                    curNum6--;
-                    nbtn6.setText(curNum6+"");
-                }
-            }
-        });
-        pbtn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(curNum6>=20){
-                    nbtn6.setText("20");
-                }else{
-                    curNum6++;
-                    nbtn6.setText(curNum6+"");
-                }
-            }
-        });
-
-        //출발지 및 도착지 선택
-        button_arrival=(Button) findViewById(R.id.button6);
-        button_arrival.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),travelActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        //출발일시
-        button_date=(Button) findViewById(R.id.button4);
-        button_date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),StartActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
-    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
-            FragmentTransaction transaction=fragmentManager.beginTransaction();
-            switch (menuItem.getItemId()){
-                case R.id.bottom_ticketing:
-                    transaction.replace(R.id.container,fragment).commitAllowingStateLoss();
-                    break;
-                case R.id.bottom_register:
-                    transaction.replace(R.id.container,fragment_second).commitAllowingStateLoss();
-                    break;
-            }
-            return true;
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
